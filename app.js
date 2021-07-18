2ff9b7ea12d4e59be313d9545a0bd684c5b1e242
arshak-avagyan <75394112+arshak-avagyan@users.noreply.github.com>
2021-07-18 17:38:16 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,53 +1,81 @@
-let users = []
-let msgs = []
-
-const Events = require('events')
-const Logger = new Events()
-
-Logger.on('message', (msg) => {
-    console.log('New message: ' + msg)
-    msgs.push(msg)
-})
+const path = require('path')
+const fs = require('fs')
+const readline = require('readline')
+const csvtojson = require("csvtojson");
 
 
-Logger.on('login', (name) => {
-    console.log('New user: ' + name)
-    users.push(name)
-})
+/////////////////////////////// Task 1 ///////////////////////////////
 
-Logger.on('getUsers', () => {
-    console.log('Logged users ')
-    for(let i of users) {
-        console.log(i)
-    }
-})
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// })
 
-Logger.on('getMessages', () => {
-    console.log('Messages ')
-    for(let i of msgs) {
-        console.log(i)
-    }
-})
+// function read() {
+//     rl.question('Please type the address to read the file \n', (userInput) => {
+//         if(fs.existsSync(userInput)) {
+//             fs.readFile(userInput, (err, data) => {
+//                 if(err) {
+//                     console.log('error: ' + err)
+//                     read()
+//                 } else {
+//                     console.log(data.toString())
+//                     rl.close()
+//                 }
+//             })
+//         } else {
+//             console.log('Wrong directory')
+//             read()
+//         }
+//     })
+// }
 
-Logger.emit('message', 'Hello world')
-Logger.emit('message', 'Good morning')
-Logger.emit('login', 'Arshak')
-Logger.emit('login', 'John')
-Logger.emit('getUsers')
-Logger.emit('getMessages')
+// rl.on('close', () => {
+//     console.log('Successful!!!')
+// })
 
-const argv = require('minimist')(process.argv.slice(2))
+// read()
 
-addUser(argv.addUser)
-message(argv.message)
+/////////////////////////////// Task 2 ///////////////////////////////
 
-function addUser (name) {
-    if(name) users.push(name)
-}
+// const dir = path.join('test', 'addresses.csv') 
+// csvtojson().fromFile(dir).then(data => {
+//     console.log(data)
+// })
 
-function message (message) {
-    if(message) msgs.push(message)
-}
 
-// console.log(users)
-// console.log(msgs)
+
+/////////////////////////////// Task 3 ///////////////////////////////
+
+
+///////////////Unzip///////////////////
+
+// const decompress = require('decompress');
+
+// (async () => {
+//     try {
+//         const files = await decompress("test/node_modules.zip", "test/unzipped")
+//         console.log('done')
+//     } catch (error) {
+//         console.log(error)
+//     }
+// })()
+
+
+// const startPoint = 'test/unzipped/node_modules';
+// let deep = 0;
+
+// function generateStructure(dir, deep) {
+//     fs.readdirSync(dir).forEach((file) => {
+//         let fullPath = path.join(dir, file);
+//         if (fs.lstatSync(fullPath).isDirectory()) {
+//             console.log(" ".repeat(deep) + '|__' + file)
+//             generateStructure(fullPath, deep+2);
+//         } else {
+//             console.log(" ".repeat(deep) + '|__' + file)
+//             //Do some code
+//         }
+//     });
+// }
+
+// generateStructure(startPoint, deep)
---
+++ data.js
@@ -0,0 +1,3 @@
+const text = 'This is the data from data.js'
+
+module.exports = text
